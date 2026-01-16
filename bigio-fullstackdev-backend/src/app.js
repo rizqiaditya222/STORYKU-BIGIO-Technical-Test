13b10b68487c7d754b2c646493c522dc9fd1ab75
Rizqi <rizqiaditya222@gmail.com>
2026-01-16 16:41:13 +0700

    fix: adding cors origin to rizqiaditya.com

--- bigio-fullstackdev-backend/src/app.js
+++ bigio-fullstackdev-backend/src/app.js
@@ -6,7 +6,10 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://storyku.rizqiaditya.com',
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
